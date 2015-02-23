d8bccfff9fb8941aa067037376acd617ca9a4298
Dhanesh Arole <dhanesha@thoughtworks.com>
2015-02-23 11:31:44 +0530

    Add price method on movie, takes daysRented as argument

--- src/bootcamp/Movie.java
+++ src/bootcamp/Movie.java
@@ -26,4 +26,25 @@ public class Movie {
 		this.priceCode = priceCode;
 	}
 
+    public double price(Integer daysRented) {
+        double thisAmount = 0;
+        switch (priceCode) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (daysRented > 2) {
+                    thisAmount += (daysRented - 2) * 1.5;
+                }
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (daysRented > 3) {
+                    thisAmount += (daysRented - 3) * 1.5;
+                }
+                break;
+        }
+        return thisAmount;
+    }
 }
